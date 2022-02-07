db1302c4b3df1be76bdeb947dc31664d519b279d
Drew Grey <dtsgrey@gmail.com>
2022-02-07 10:56:16 -0500

    commit tree 25cc714cf35b82e394e200c531b53edabe6bf0a3

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution here
+function findMatching(driverArr,str){
+    let matchingDriver = driverArr.filter( function(user) {
+     if(user.toLowerCase() === str.toLowerCase()){
+         return user;
+     } 
+    })
+    return matchingDriver;
+}
+
+function fuzzyMatch(driverArr,str){
+    let matchingDriver = driverArr.filter(function(user){
+        if(user.startsWith(str)){
+            return user;
+        }
+    })
+    return matchingDriver;
+}
+
+function matchName(driverArr,str){
+    let matchingEl = driverArr.filter(function(user){
+        if(str === user.name){
+            return user
+        }
+    })
+    return matchingEl;
+}
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+console.log(matchName(drivers, `Bobby`));
